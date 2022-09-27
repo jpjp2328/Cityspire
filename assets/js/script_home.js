1eeeb6336e055f6e65342cf0720fde8658a9c183
505laura <106522200+505laura@users.noreply.github.com>
2022-09-27 17:49:18 +1000

    Fix modal not appearing on page load

--- assets/js/script_home.js
+++ assets/js/script_home.js
@@ -123,17 +123,19 @@ function randomPlaces(photoEl1, photoEl2, photoEl3) {
     findPlace(randomCountry3, photoEl3);
 }
 
-var destinationBtn = document.getElementById('destinationBtn');
-var destinationContent = document.getElementById('destinationContent');
-var closeBtn = document.getElementById('closeBtn');
-var overlay = document.getElementById('overlay');
-
-destinationBtn.addEventListener('click', function () {
-    destinationContent.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-});
-
-closeBtn.addEventListener('click', function () {
-    destinationContent.classList.add('hidden');
-    overlay.classList.add('hidden');
-});
+window.onload = function() {
+    var destinationBtn = document.getElementById('destinationBtn');
+    var destinationContent = document.getElementById('destinationContent');
+    var closeBtn = document.getElementById('closeBtn');
+    var overlay = document.getElementById('overlay');
+    
+    destinationBtn.addEventListener('click', function () {
+        destinationContent.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
+    
+    closeBtn.addEventListener('click', function () {
+        destinationContent.classList.add('hidden');
+        overlay.classList.add('hidden');
+    });
+}
